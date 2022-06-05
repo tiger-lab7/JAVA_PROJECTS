17b1a7a87a55d683ffe68d63ec1b51c9383ca3ab
IgorSamoylov <igorletterbox@gmail.com>
2022-06-05 19:31:59 +0500

    Next

--- ДВУХСТЕКОВЫЙ АЛГОРИТМ ДЕЙКСТРЫ ДЛЯ ВЫЧИСЛЕНИЯ ВЫРАЖЕНИЙ/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String... args) {
-
-        String expr = "(1-(777+3989)/287)*568796";
-        System.out.println(Evaluate.evaluate(expr));
-    }
-}
-
-
